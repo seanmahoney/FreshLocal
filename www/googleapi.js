dc2fc3ee9a84b5b5898b1090260fd3dabffdd8bf
LiBowen <renmendou@gmail.com>
2017-10-23 16:45:51 -0300

    Google Api added

---
+++ www/googleapi.js
@@ -0,0 +1,17 @@
+function initMap() {
+    var position = { lat: 44.638578, lng: -63.575649 };
+    var market1 = { lat: 44.640714, lng: -63.566272 };
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('google_map'), {
+        center: position,
+        zoom: 13
+    });
+    var marker = new google.maps.Marker({
+        position: market1,
+        map: map,
+        title: 'Hello World!'
+    });
+    marker.addListener('click', function() {
+        window.location.replace("secondpage.html");
+    });
+}
