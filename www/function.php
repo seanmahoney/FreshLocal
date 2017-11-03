bb620690ba7b914d3af6a7cce1d09a32c9edc710
LiBowen <renmendou@gmail.com>
2017-11-03 16:42:29 -0300

    css fix

--- www/function.php
+++ www/function.php
@@ -1,13 +1,16 @@
 <?php
   function get_header(){
-  	login_corner();
+  	echo "<div class='header'>";
+  	get_logo();
+  	
     echo "<ul class='main_navigation'>";
 	echo "<li class='main_navigation_item'><a class='main_navigation_link active' href='#home'>Home</a></li>";
 	echo "<li class='main_navigation_item'><a class='main_navigation_link' href='#news'>Product</a></li>";
 	echo "<li class='main_navigation_item'><a class='main_navigation_link' href='#contact'>Map</a></li>";
 	echo "<li class='main_navigation_item'><a class='main_navigation_link' href='#about'>Contact</a></li>";
 	echo "</ul>";
-	get_logo();
+	login_corner();
+	echo "</div>";
   }
   function login_corner(){
   	echo "<ul class='secondary_navigation'>";
