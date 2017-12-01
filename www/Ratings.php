ec966d9af1bc356bcfcc2b863473179896ffd15c
lenovo <yn739843@dal.ca>
2017-11-30 20:54:52 -0400

    merged CSS

--- www/Ratings.php
+++ www/Ratings.php
@@ -27,24 +27,24 @@
 			<div id="mainArea">
 				<h2>Rate</h2>
 				<div id="farmerName" name="farmerName"></div>
-				<form method="post" action=<?php echo 'commentSubmit.php?farmerID='.$farmerID; ?>>
 				<div id="textBox">
-					<input type="textArea" id = "text" placeholder="Comments" name="comment">  
-              		</input>
+					<textarea id = "text" placeholder="Comments">  
+              		</textarea>
 				</div>
 				<h3>Rating</h3>
 				<div id="stars">
-					 
-    				<input type="radio" name="group-1" id="group-1-0" value="5" /><label for="group-1-0"></label>
-    				<input type="radio" name="group-1" id="group-1-1" value="4" /><label for="group-1-1"></label>
-    				<input type="radio" name="group-1" id="group-1-2" value="3" /><label for="group-1-2"></label>
-    				<input type="radio" name="group-1" id="group-1-3" value="2" /><label for="group-1-3"></label>
-    				<input type="radio" name="group-1" id="group-1-4"  value="1" /><label for="group-1-4"></label>
-    				<input type= "submit" id="submit" value= "Submit">
-    			</form>
+					<form method="post" action="<?php echo 'commentSubmit.php?farmerID='.$farmerID; ?>">					 
+	    				<input type="radio" name="group-1" id="group-1-0" value="5" /><label for="group-1-0"></label>
+	    				<input type="radio" name="group-1" id="group-1-1" value="4" /><label for="group-1-1"></label>
+	    				<input type="radio" name="group-1" id="group-1-2" value="3" /><label for="group-1-2"></label>
+	    				<input type="radio" name="group-1" id="group-1-3" value="2" /><label for="group-1-3"></label>
+	    				<input type="radio" name="group-1" id="group-1-4"  value="1" /><label for="group-1-4"></label>
+	    				<input type= "submit" id="submit" value= "Submit"/>
+	    			</form>
 				</div>
-				
-				
+				<br>
+				<br>
+				<br>
 			</div>	
 		</div>
 		<script type="text/javascript" src="js/Ratings.js"></script>
--- www/registrationFarmer.php
+++ www/registrationFarmer.php
@@ -5,7 +5,7 @@
 	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Registration System</title>
-
+	<link rel="stylesheet" type="text/css" href="css/farmerLogin.css">
 
 </head>
 <body>
@@ -15,31 +15,31 @@
 			<form method="POST" action="registrationCheck.php">
 				<table align="center" width="30%" border="0">
 					<tr>
-						<td><input type="text" name="username" placeholder="User Name" required /></td>
+						<td><input type="text" name="username" placeholder="User Name" class="field" required /></td>
 					</tr>
 					<tr>
-						<td><input type="text" name="phonenumber" id="phonenumber" placeholder="Phone Number"></td>
+						<td><input type="text" name="phonenumber" id="phonenumber" class="field" placeholder="Phone Number"></td>
 					</tr>
 					<tr>
-						<td><input type="text" name="email" placeholder="Your Email" required /></td>
+						<td><input type="text" name="email" placeholder="Your Email" class="field" required /></td>
 					</tr>
 					<tr>
-						<td><input type="password" name="password" placeholder="Your Password" required /></td>
+						<td><input type="password" name="password" placeholder="Your Password" class="field" required /></td>
 					</tr>
 					<tr>
-						<td><input type="url" name="video" id="video" placeholder="Video Link" required/></td>
+						<td><input type="url" name="video" id="video" placeholder="Video Link" class="field" required/></td>
 					</tr>
 					<tr>
-						<td><textarea rows="5" cols="50" name="FAQ" id="FAQ" placeholder="FAQ"></textarea></td>
+						<td><textarea rows="5" cols="50" name="FAQ" id="FAQ" placeholder="FAQ" class="field"></textarea></td>
 					</tr>   
 					<tr>
-						<td><textarea rows="4" cols="50" name="profile" id="profile" placeholder="Profile" placeholder="FAQ"></textarea></td>
+						<td><textarea rows="4" cols="50" name="profile" id="profile" placeholder="Profile" class="field"></textarea></td>
 					</tr>
 					<tr> 
-						<td><input type="submit" name="signup" value ="Sign Me Up"/></td>
+						<td><input type="submit" name="signup" id="submit" value ="Sign Me Up"/></td>
 					</tr>
 					<tr>
-						<td><a href="login.php">Sign In Here</a></td>
+						<td><a href="login.php" id="link">Sign In Here</a></td>
 					</tr>
 					<input type="hidden" name="value" value="farmer">
 				</table>
