746db8f2deea9046d5651bd71d70dada789464ba
ramikhashmel <Ramster@Ramis-MacBook-Pro.local>
2017-12-11 23:36:16 -0400

    Admin Panel

--- adminFarmer.php
+++ adminFarmer.php
@@ -13,45 +13,43 @@
 ?>
 <!DOCTYPE html>
 <html>
-     
 <head>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/custom.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
 	<link rel="stylesheet" type="text/css" href="css/admin.css">
+	
 	<title>Admin Panel</title>
 	
-	<script>
-function trueFunction()
+<script>
+	function trueFunction()
 {
-alert("User has been deleted!"); // this is the message in ""
+	alert("Farmer has been deleted!"); // this is the message in ""
 }
 
-
-
 </script>
+
 
 </head>
 <body>
 
 <ul>
-  <li><a href="admin.php">Users</a></li>
-  <li><a href="adminFarmer.php">Farmers</a></li>
+  <li><a href="admin.php">Users</a></li> 
+  <li><a href="adminFarmer.php">Farmers</a></li> 
 </ul>
-
-
-
 <h1 style ="text-align: center;">Welcome to the Admin Panel</h1>
 
 <table width="1000" border="5" align="center">
 <tr>
-		<td colspan="4" align="center" bgcolor="yellow"><h1> All Users</h1></td>
+		<td colspan="4" align="center" bgcolor="#4CAF50"><h1> All Farmers</h1></td>
 </tr>
 
-<tr><form action = "admin.php" method = "post">
+<tr><form action = "adminFarmer.php" method = "post">
 		<th>User ID</th>
-		<th>Username</th>
+		<th>Name</th>
+		<th>Profile</th>
+		<th>Phone Number</th>
 		<th>Email</th>
-		<th>Delete post</th>
+		<th>Video link</th>
 	
 		
 </tr>
@@ -63,22 +61,30 @@ alert("User has been deleted!"); // this is the message in ""
 
 include_once("config.php");
 	
-	$sql = "SELECT * FROM `freshLocal_users` ORDER BY userId DESC";
+	$sql = "SELECT * FROM `Farmers` ORDER BY id DESC";
 	$query = mysqli_query($con, $sql);
 
 	
 	while ($row = mysqli_fetch_array($query)) {
-		$id = $row['userId'];
-		$username= $row['userName'];
-		$email = $row['email'];
-
+		$id = $row['ID'];
+		$name= $row['Name'];
+		$profile = $row['Profile'];
+		$phone = $row['PhoneNumber'];
+		$email = $row['Email'];
+		$video = $row['Video_link'];
+		$faq = $row['FAQ'];
+		
 	
 ?>
 
 <tr>
 	<td><?php echo $id; ?></td>
-	<td><?php echo $username; ?></td>
+	<td><?php echo $name; ?></td>
+	<td><?php echo $profile; ?></td>
+	<td><?php echo $phone; ?></td>
 	<td><?php echo $email; ?></td>
+	<td><?php echo $video; ?></td>
+	<td><?php echo $faq; ?></td>
 
 	<td><input type ="submit" onclick="trueFunction()" id="submit" name= "submit" value="Delete" required></td>
 
@@ -96,7 +102,7 @@ include_once("config.php");
 	//DELETE QUERY
 	if ($_POST['submit']) {
 	   include_once("config.php");
-	   mysqli_query($con, "DELETE FROM `freshLocal_users` WHERE userId = $id");
+	   mysqli_query($con, "DELETE FROM `Farmers` WHERE ID = $id");
 		
 		echo '<script>truefunction()</script>';
 		header("Refresh:0");
