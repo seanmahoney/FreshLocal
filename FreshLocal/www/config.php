0a13ffdbadee345d0fa3c4b9d1c6e05c0f50aab3
lenovo <yn739843@dal.ca>
2017-12-11 23:42:11 -0400

    Delete an old version

--- FreshLocal/www/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("db.cs.dal.ca", "bli", "B00692240", "bli");
-
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-?>
--- FreshLocal/www/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$dbCon = mysqli_connect("localhost", "root", "localfresh", "localfresh") or die (mysqli_error());
-	?>
